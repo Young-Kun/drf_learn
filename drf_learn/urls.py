824a6c12b0764bfcfdeffc293443d38c47520a02
Youngkun <youngkunzhu@163.com>
2020-02-10 14:55:28 +0800

    init

--- drf_learn/urls.py
+++ drf_learn/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path, include
+from django.contrib import admin
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', include('rest_framework.urls')),
-    path('', include('snippets.urls'))
+    path('', include('user.urls'))
 ]
