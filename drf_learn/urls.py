3ec7b4e82becbae07142bc77b44595a817f4cb60
Youngkun <youngkunzhu@163.com>
2020-02-10 10:49:52 +0800

    view 完毕 ，开始修改模型

--- drf_learn/urls.py
+++ drf_learn/urls.py
@@ -16,5 +16,6 @@ Including another URLconf
 from django.urls import path, include
 
 urlpatterns = [
-    path('api-auth/', include('rest_framework.urls'))
+    path('', include('rest_framework.urls')),
+    path('', include('snippets.urls'))
 ]
