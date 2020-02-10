9041f08563a6baac1d892c5ee5ab3268aa86a4c4
Youngkun <youngkunzhu@163.com>
2020-02-10 20:07:23 +0800

    init

--- user/views.py
+++ user/views.py
@@ -1,8 +1,14 @@
 from .models import Post
 from .serializers import PostSerializer
-from rest_framework import generics
+from rest_framework import generics, mixins
+from rest_framework.mixins import Response
 
 
 class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
