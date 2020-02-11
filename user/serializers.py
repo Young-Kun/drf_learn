d5cf9402df205c32897ad834a1234796d7a956ed
Youngkun <youngkunzhu@163.com>
2020-02-11 14:39:22 +0800

    finished

--- user/serializers.py
+++ user/serializers.py
@@ -1,20 +1,20 @@
-from rest_framework import serializers
+from rest_framework import serializers, settings, fields
 from .models import Post, User, Topic
 
 
 class PostSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Post
-        fields = ['url', 'title', 'content', 'created', 'user', 'topics']
+        fields = ['url', 'id', 'title', 'content', 'created', 'user', 'topics']
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username']
+        fields = ['url', 'id', 'username', 'post_set']
 
 
 class TopicSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Topic
-        fields = ['id', 'name']
+        fields = ['url', 'id', 'name']
