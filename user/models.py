ba880ed715565e4232987b73b1a23853e26fbd68
Youngkun <youngkunzhu@163.com>
2020-02-11 10:38:02 +0800

    finished

--- user/models.py
+++ user/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 
 
+class User(models.Model):
+    username = models.CharField(max_length=128)
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=128)
+
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    topics = models.ManyToManyField(Topic, blank=True)
 
     class Meta:
-        ordering = ['created']
+        ordering = ['-created']
