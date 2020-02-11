8958d2c94a36a6bb3988c38d40192f192a752fb7
Youngkun <youngkunzhu@163.com>
2020-02-11 20:53:39 +0800

    finished

--- user/views.py
+++ user/views.py
@@ -1,11 +1,14 @@
 from .models import Post, User, Topic
 from .serializers import PostSerializer, UserSerializer, TopicSerializer
 from rest_framework import viewsets, permissions, serializers, settings
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['title']
 
 
 class UserViewSet(viewsets.ModelViewSet):
